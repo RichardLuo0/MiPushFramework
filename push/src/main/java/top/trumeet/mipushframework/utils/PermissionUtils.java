3e8a9b04d6ef846c7cdd44d0de9917dffff41a3b
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2025-03-08 02:54:30 +0800

    rebase merge

--- push/src/main/java/top/trumeet/mipushframework/utils/PermissionUtils.java
+++ push/src/main/java/top/trumeet/mipushframework/utils/PermissionUtils.java
@@ -16,50 +16,31 @@
 
 package top.trumeet.mipushframework.utils;
 
-import android.app.AppOpsManager;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.widget.Toast;
-
-import com.xiaomi.xmsf.R;
-
-import top.trumeet.common.Constants;
-import top.trumeet.common.utils.Utils;
 
 public class PermissionUtils {
-    public static boolean canAssignPermissionViaAppOps() {
-        return Utils.isAppOpsInstalled() || ShellUtils.isSuAvailable();
-    }
-
     public static boolean lunchAppOps(Context context, String permission, CharSequence tips) {
-        // root first
-        if (ShellUtils.isSuAvailable()) {
-            if (allowPermission(permission)) {
-                return true;
-            } else {
-                Toast.makeText(context, R.string.fail, Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        if (Utils.isAppOpsInstalled()) {
-            Intent intent = new Intent(Intent.ACTION_SHOW_APP_INFO)
-                    .setClassName("rikka.appops", "rikka.appops.appdetail.AppDetailActivity")
-                    .putExtra("rikka.appops.intent.extra.USER_HANDLE", Utils.myUid())
-                    .putExtra("rikka.appops.intent.extra.PACKAGE_NAME", Constants.SERVICE_APP_NAME)
-                    .setData(Uri.parse("package:" + Constants.SERVICE_APP_NAME))
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent);
-            Toast.makeText(context, tips, Toast.LENGTH_LONG).show();
-            return true;
-        }
+        //        // root first
+        //        if (ShellUtils.isSuAvailable()) {
+        //            if (allowPermission(permission)) {
+        //                return true;
+        //            } else {
+        //                Toast.makeText(context, R.string.fail, Toast.LENGTH_SHORT).show();
+        //            }
+        //        }
+        //
+        //        if (Utils.isAppOpsInstalled()) {
+        //            Intent intent = new Intent(Intent.ACTION_SHOW_APP_INFO)
+        //                    .setClassName("rikka.appops", "rikka.appops.appdetail.AppDetailActivity")
+        //                    .putExtra("rikka.appops.intent.extra.USER_HANDLE", Utils.myUid())
+        //                    .putExtra("rikka.appops.intent.extra.PACKAGE_NAME", Constants.SERVICE_APP_NAME)
+        //                    .setData(Uri.parse("package:" + Constants.SERVICE_APP_NAME))
+        //                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //            context.startActivity(intent);
+        //            Toast.makeText(context, tips, Toast.LENGTH_LONG).show();
+        //            return true;
+        //        }
 
         return false;
-    }
-
-    public static boolean allowPermission(String permission) {
-        return ShellUtils.exec("appops set --user " + Utils.myUid() +
-                " " + Constants.SERVICE_APP_NAME + " " + permission +
-                " " + AppOpsManager.MODE_ALLOWED);
     }
 }
